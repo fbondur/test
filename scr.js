47d79312ae640ac3f98e649be96f482c41bf0e68
Forest <fbondu@ya.ru>
2017-06-14 17:25:44 +0300

    Changes #6

--- scr.js
+++ scr.js
@@ -5,6 +5,7 @@ function sum(a, b) {
     return a + b;
 }
 console.log(sum(x, 22));
+<<<<<<< 62ea83147702033463d6e542656cf4a7bf48a7e8
 console.log("fsdakfj");
 let j = 25;
 console.log(x+j);
@@ -14,3 +15,4 @@ wants
 to
 be
 added
+Changes #6
