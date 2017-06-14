388d60944dfff5cc1082fe783c0cfe8152835ee9
Forest <fbondu@ya.ru>
2017-06-14 17:38:28 +0300

    Changes #8

--- scr.js
+++ scr.js
@@ -5,14 +5,8 @@ function sum(a, b) {
     return a + b;
 }
 console.log(sum(x, 22));
-<<<<<<< 62ea83147702033463d6e542656cf4a7bf48a7e8
 console.log("fsdakfj");
 let j = 25;
 console.log(x+j);
 some
 text
-wants
-to
-be
-added
-Changes #6
